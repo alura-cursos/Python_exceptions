d45de81895c469bbed2c152ca08563b90326d155
Ricardo <dones.bugan@gmail.com>
2020-03-26 10:19:03 -0300

    video 1.4

---
+++ exemple.py
@@ -0,0 +1,8 @@
+def dividir(dividendo, divisor):
+    return dividendo/divisor
+
+def testa_divisao(divisor):
+    resultado = dividir(10,divisor)
+    print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
+
+testa_divisao(0)
