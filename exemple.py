c1db5c84525a9650ce1aae5fd1963df3ee7505bf
Ricardo <dones.bugan@gmail.com>
2020-03-26 10:26:22 -0300

    video 2.1

--- exemple.py
+++ exemple.py
@@ -3,15 +3,14 @@ def dividir(dividendo, divisor):
     return dividendo/divisor
 
 def testa_divisao(divisor):
-    try:
         resultado = dividir(10,divisor)
         print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
-    except ZeroDivisionError:
-        print ("Erro de divisão por 0 tratado")
 
 try:
     testa_divisao(0)
-except AttributeError:
+except ZeroDivisionError as E:
     print("Erro de atributo tratado")
+except Exception as E:
+    print("Tratamento genérico")
 
 print("Programa encerrado")
